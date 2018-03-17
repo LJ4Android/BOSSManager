a1598b84fa18175b5a520aaa0a707aefea6c834d
wuhan <542419456@qq.com>
2018-03-17 14:42:48 +0800

    组件化
    Signed-off-by: wuhan <542419456@qq.com>

---
+++ uploadassistant/src/main/java/com/lj/uploadassistant/MainActivity.java
@@ -0,0 +1,10 @@
+package com.lj.uploadassistant;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wh on 2018/3/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
